cb51a0d0cc9860d3b982515a0be34476fca15bcb
HrachDaniel <daniel.grach06@gmail.com>
2025-05-26 16:26:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -100,4 +100,24 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Ініціалізація слайдера
     initSlider();
+
+
+const hamburgerBtn = document.querySelector('.hamburger-menu');
+const navMenu = document.querySelector('.header .menu');
+
+    if (hamburgerBtn && navMenu) {
+        hamburgerBtn.addEventListener('click', () => {
+            navMenu.classList.toggle('active');
+            hamburgerBtn.classList.toggle('active');
+        });
+
+        navMenu.querySelectorAll('a').forEach(item => {
+            item.addEventListener('click', () => {
+                if (navMenu.classList.contains('active')) {
+                    navMenu.classList.remove('active');
+                    hamburgerBtn.classList.remove('active');
+                }
+            });
+        });
+    }
 });
